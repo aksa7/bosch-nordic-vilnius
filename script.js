cddac5c8e46ee3e943a2523cb0554c639a6129d5
aksa7 <jaksamitauskas@gmail.com>
2025-04-28 17:35:33 +0300

    Initial commit with .gitignore

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-// Smooth scroll for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth"
-      });
-    });
-  });
+
+function showDay(evt, dayId) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(dayId).style.display = "block";
+  evt.currentTarget.classList.add("active");
+}
